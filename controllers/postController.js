78844fb0297271dfe228ef774eef1c8a688548d7
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-08 23:46:12 +0530

    Implement pagination for fetching posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,9 +2,16 @@ const Post = require('../models/Post');
 
 exports.getPosts = async (req, res) => {
   try {
-    // Fetch posts data from database (implement pagination if needed)
-    const posts = await Post.find().exec();
-    res.json({ success: true, posts });
+    const page = parseInt(req.query.page) || 1;
+    const perPage = 6;
+    const skip = (page - 1) * perPage;
+
+    // Fetch posts data from database with pagination
+    const posts = await Post.find().skip(skip).limit(perPage).exec();
+
+    const totalPages = await Post.countDocuments();
+
+    res.json({ success: true, posts , totalPages });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: 'Internal server error' });
